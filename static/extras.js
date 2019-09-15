46c913d88eb8be8280af7cafd6e5e99a2239c6b0
Tina Shi <tshi@umail.ucsb.edu>
2019-09-14 23:02:27 -0400

    added canvas and combo

---
+++ static/extras.js
@@ -0,0 +1,87 @@
+
+var comboMultiplier = 1;
+var comboCount = 0;
+var missCount = 0;
+var ww = window.innerWidth;
+var wh = window.innerHeight;
+var gamePieces = [];
+
+function calculateMultipliedPoints(pointValue) {
+	if (missCount > 1) {
+		comboCount = 0;
+		comboMultiplier = 1;
+		missCount = 0;
+	}
+	comboMultiplier += Math.floor(comboCount / 2) * .1;
+	return pointValue * comboMultiplier;
+}
+
+window.onload = function() {
+	startGame();
+}
+
+function startGame() {
+	gameArea.start();
+	var leftPlayer = new component(982, 1012, 10, 10, 
+		"/assets/Robot_PNG/PNG_Animations/Robot1/01_idle/idle_000.png",
+		"image");
+	gamePieces.push(leftPlayer);
+}
+
+var gameArea = {
+	canvas: document.createElement("canvas"),
+	start: function() {
+		this.canvas.width = window.innerWidth;
+		this.canvas.height = window.innerHeight;
+		this.context = this.canvas.getContext("2d");
+		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		this.interval = setInterval(updateGameArea, 1000 / 60);
+	},
+	clear: function() {
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	}
+}
+
+function component(width, height, x, y, color, type) {
+	this.type = type;
+	this.width = width;
+	this.height = height;
+	this.x = x;
+	this.y = y;
+	switch(this.type) {
+		case "image":
+			console.log("image");
+			this.image = new Image();
+			this.image.src = color;
+			break;
+		case "text":
+			ctx.font = this.width + " " + this.height;
+			ctx.fillStyle = color;
+			ctx.fillText(this.text, this.x, this.y);
+			break;
+	}
+	this.update = function() {
+		ctx = gameArea.context;
+		switch (this.type) {
+			case "image":
+				ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+				break;
+			case "text":
+				ctx.font = this.width + " " + this.height;
+				ctx.fillStyle = color;
+				ctx.fillText(this.text, this.x, this.y);
+				break;
+		}
+	}
+}
+
+function updateGameArea() {
+	gameArea.clear();
+	updateGamePieces();
+}
+
+function updateGamePieces() {
+	for (var i = 0; i < gamePieces.length; i++) {
+		gamePieces[i].update();
+	}
+}
